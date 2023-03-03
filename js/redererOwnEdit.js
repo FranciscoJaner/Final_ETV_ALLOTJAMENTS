fccac9a1226d2bc030fa0cfd29d381664270ab4c
aaguus01 <sergioagustinparedes@gmail.com>
2023-03-03 18:24:50 +0100

    Login arreglado, todo centrado

--- js/redererOwnEdit.js
+++ js/redererOwnEdit.js
@@ -22,7 +22,7 @@ ipcRenderer.on("enviar-edit-mod", function (e, info, id) {
     let name = $(`<p>${_element.nom}</p>`);
     let descripcion = $(`<p>${_element.descripcio}</p>`);
     let botonEdit = $(
-      `<button type="button" id="editButton" onclick='modificarCasa(${_element.id}, ${_element.propietari_id})'>EDIT</button>`
+      `<button type="button" id="editButton" onclick="modificarCasa(${_element.id}, ${_element.propietari_id})">EDIT</button>`
     );
     divPrincipal.append(name, descripcion, botonEdit);
     divPrincipalInject.append(divPrincipal);
@@ -31,10 +31,10 @@ ipcRenderer.on("enviar-edit-mod", function (e, info, id) {
 
 // Envia id al otro renderer.
 function modificarCasa(id, propietari_id) {
-  console.log("Modificando Casa, id: " + id);
   ipcRenderer.send("give-id", id);
   if (user_id == propietari_id) {
     ipcRenderer.send("editwindow")
+    
   } else {
     dialog.showMessageBox({
       title: "Warning",
