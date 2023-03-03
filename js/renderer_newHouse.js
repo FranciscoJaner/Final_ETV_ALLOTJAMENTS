85c8681c1da90cf64ccf7573afac2659605cab71
aaguus01 <sergioagustinparedes@gmail.com>
2023-03-03 18:55:47 +0100

    cambios

--- js/renderer_newHouse.js
+++ js/renderer_newHouse.js
@@ -1,4 +1,4 @@
-const { ipcRenderer } = require("electron");
+const { ipcRenderer , dialog} = require("electron");
 
 let form = document.getElementById("newhouseForm");
 
@@ -42,8 +42,15 @@ form.addEventListener("submit", function () {
     latitud: latitude,
   };
 
+  //Mostramos un mensaje de que se ha creado correctamente la casa
+  dialog.showMessageBox({
+    type: "info",
+    message:
+        "Casa creada correctamente",
+  });
   // Enviamos al main todos los datos.
   ipcRenderer.send("insert-house", newHouse);
+
 });
 
 ipcRenderer.on("insert-finished", function (e) {
